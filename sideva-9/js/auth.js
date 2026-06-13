94f84d5161304687c208ef8f17fac0f16e4049ca
setmtq33-crypto <set.mtq33@gmail.com>
2026-06-13 14:31:45 +0700

    Update auth.js

--- sideva-9/js/auth.js
+++ sideva-9/js/auth.js
@@ -23,3 +23,33 @@ async function login() {
     window.location.href =
         'dashboard.html';
 }
+
+async function getProfile() {
+
+    const {
+        data: { user }
+    } = await supabaseClient.auth.getUser();
+
+    if (!user) {
+
+        window.location.href = "/pages/login";
+        return null;
+    }
+
+    const { data, error } =
+        await supabaseClient
+            .from('profiles')
+            .select('*')
+            .eq('auth_user_id', user.id)
+            .single();
+
+    if (error) {
+
+        console.error(error);
+        return null;
+    }
+
+    return data;
+}
+
+window.getProfile = getProfile;
