5c9e68b6429928c486c85dda8a9d171db22ba2a3
setmtq33-crypto <set.mtq33@gmail.com>
2026-06-17 22:16:27 +0700

    Add 'menuRup' and 'menuPackages' to role menus

--- sideva-9/js/layout.js
+++ sideva-9/js/layout.js
@@ -67,7 +67,7 @@ async function loadLayout() {
                     'menuAccounts',
                     'menuBidangs',
                     'menuSipd',
-                    'menuRup'
+                    
                 ],
             
                 PBJ: [
@@ -77,6 +77,7 @@ async function loadLayout() {
                     'menuAccounts',
                     'menuBidangs',
                     'menuSipd'
+                    'menuRup'
                 ],
             
                 VIEWER: [
@@ -87,6 +88,7 @@ async function loadLayout() {
                     'menuBidangs',
                     'menuSipd',
                     'menuRup'
+                    'menuPackages'
                 ]
             };
 
