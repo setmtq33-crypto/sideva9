1c5069d3b40700312429ddf453a488298e204ca4
setmtq33-crypto <set.mtq33@gmail.com>
2026-06-18 19:31:41 +0700

    Refactor sendTelegramMessage function syntax

--- sideva-9/js/telegram.js
+++ sideva-9/js/telegram.js
@@ -4,13 +4,11 @@ const TELEGRAM_BOT_TOKEN =
 const TELEGRAM_CHAT_ID =
 '8083514379';
 
-async function sendTelegramMessage(
-    text
-){
+async function sendTelegramMessage(text){
 
     try{
 
-        await fetch(
+        const response = await fetch(
             `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
             {
                 method:'POST',
@@ -18,21 +16,24 @@ async function sendTelegramMessage(
                     'Content-Type':'application/json'
                 },
                 body:JSON.stringify({
-                    chat_id:
-                        TELEGRAM_CHAT_ID,
-                    text:
-                        text
+                    chat_id: TELEGRAM_CHAT_ID,
+                    text: text
                 })
             }
         );
 
+        const result =
+            await response.json();
+
+        console.log(result);
+
+        return result;
+
     }catch(err){
 
-        console.error(
-            'Telegram Error',
-            err
-        );
+        console.error(err);
 
+        return err;
     }
 }
 
