b419dd71d49aaca65d5d0e329a6521541d64f41b
setmtq33-crypto <set.mtq33@gmail.com>
2026-06-18 19:34:26 +0700

    Refactor sendNotification function
    Refactor sendNotification function to remove unused parameters and streamline notification sending.

--- sideva-9/js/supabase.js
+++ sideva-9/js/supabase.js
@@ -4,23 +4,33 @@ window.supabaseClient = window.supabase.createClient(
 );
 
 async function sendNotification(
-    eventType,
-    title,
-    message,
-    createdBy = '',
-    opdName = ''
-){
-
-    return await window.supabaseClient
-        .from('telegram_notifications')
-        .insert([{
-            event_type: eventType,
-            title: title,
-            message: message,
-            created_by: createdBy,
-            opd_name: opdName
-        }]);
-}
+        eventType,
+        title,
+        message
+    ){
+    
+        await window.supabaseClient
+            .from('telegram_notifications')
+            .insert([{
+                event_type: eventType,
+                title: title,
+                message: message
+            }]);
+    
+        if(
+            typeof sendTelegramMessage
+            ===
+            'function'
+        ){
+    
+            await sendTelegramMessage(
+    `🔔 ${title}
+    
+    ${message}`
+            );
+    
+        }
+    }
 
 window.sendNotification =
     sendNotification;
